2abddfb14b5e8abfa93e5b705015712962eb8fe7
Naladestruction <111301518+Naladestruction@users.noreply.github.com>
2022-08-24 15:12:37 -0500

    modificacion en clase Cls_personalSalud
    agregamos atributo Especialidad a PersonalSalud

--- HogarGestor.app/HogarGestor.app.Dominio/Cls_PersonalSalud.cs
+++ HogarGestor.app/HogarGestor.app.Dominio/Cls_PersonalSalud.cs
@@ -1,3 +1,4 @@
+using System.Dynamic;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -5,8 +6,11 @@ using System.Threading.Tasks;
 
 namespace HogarGestor.app.Dominio
 {
-    public class Cls_PersonalSalud
+    public class Cls_PersonalSalud:Cls_Persona
     {
-        
+        public int Id {get; set;}
+
+        public Especialidad Especialidad {get; set;}
+            
     }
 }
