96dd0a87b04ca53c29e85679e54435b4b3ca6589
rojecas <rojecas@gmail.com>
2022-08-29 02:11:12 -0500

    Creacion de proyecto

---
+++ HogarGestor.app/HogarGestor.App.Persistencia/Class1.cs
@@ -0,0 +1,5 @@
+﻿namespace HogarGestor.App.Persistencia;
+public class Class1
+{
+
+}
--- HogarGestor.app/HogarGestor.app.Dominio/Cls_Familiar.cs
+++ HogarGestor.app/HogarGestor.app.Dominio/Cls_Familiar.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 
 namespace HogarGestor.App.Dominio
 {
-    public class Cls_Familiar
+    public class Cls_Familiar : Cls_Persona
     {
         public int Id {get;set;}
         public string Parentesco {get;set;}
