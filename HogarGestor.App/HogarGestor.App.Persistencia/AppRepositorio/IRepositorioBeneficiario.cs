62fc67921794791e64f89d18ae1ecb7d5f5dbe9f
rojecas <rojecas@gmail.com>
2022-09-03 13:05:52 -0500

    Añadir Clases
    IRepositorioBeneficiario.cs
    RepositorioBeneficiario.cs

---
+++ HogarGestor.App/HogarGestor.App.Persistencia/AppRepositorio/IRepositorioBeneficiario.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using HogarGestor.App.Dominio;
+
+namespace HogarGestor.App.Persistencia
+{
+    public interface IRepositorioBeneficiario
+    {
+        IEnumerable<Cls_Beneficiario> GetAllBeneficiarios();
+        Cls_Beneficiario AddBeneficiario(Cls_Beneficiario beneficiario);
+        Cls_Beneficiario UpdateBeneficiario(Cls_Beneficiario beneficiario);
+        void DeleteBeneficiario(int IdBeneficiario);
+        Cls_Beneficiario GetBeneficiario(int IdBeneficiario);
+
+    }
+}
---
+++ HogarGestor.App/HogarGestor.App.Persistencia/AppRepositorio/RepositorioBeneficiario.cs
@@ -0,0 +1,28 @@
+namespace HogarGestor.App.Persistencia
+{
+    public class RepositorioBeneficiario : IRepositorioBeneficiario
+    {
+        //HogarGestor.App.Dominio.Cls_Beneficiario IRepositorioBeneficiario.AddBeneficiario(HogarGestor.App.Dominio.Cls_Beneficiario beneficiario)
+        Cls_Beneficiario IRepositorioBeneficiario.AddBeneficiario(Cls_Beneficiario beneficiario)
+        {
+            throw new System.NotImplementedException();
+        }
+        void IRepositorioBeneficiario.DeleteBeneficiario(int IdBeneficiario)
+        {
+            throw new System.NotImplementedException();
+        }
+        IEnumerable<Cls_Beneficiario> IRepositorioBeneficiario.GetAllBeneficiarios()
+        {
+            throw new System.NotImplementedException();
+        }
+        Cls_Beneficiario IRepositorioBeneficiario.GetBeneficiario(int IdBeneficiario)
+        {
+            throw new System.NotImplementedException();
+        }
+        Cls_Beneficiario IRepositorioBeneficiario.UpdateBeneficiario(Cls_Beneficiario beneficiario)
+        {
+            throw new System.NotImplementedException();
+        }
+
+    }
+}
