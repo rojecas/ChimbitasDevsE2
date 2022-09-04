488f01396f5c64fef95372be898598e1e3b4e406
Naladestruction <111301518+Naladestruction@users.noreply.github.com>
2022-09-03 22:38:11 -0500

    creacion de las clases que implementa en el crud
    iRepositorio y Repositorio

--- HogarGestor.App/HogarGestor.App.Persistencia/AppRepositorio/RepositorioBeneficiario.cs
+++ HogarGestor.App/HogarGestor.App.Persistencia/AppRepositorio/RepositorioBeneficiario.cs
@@ -1,28 +1,58 @@
+using System.Collections.Generic;
+using System.Linq;
+using HogarGestor.App.Dominio;
+
 namespace HogarGestor.App.Persistencia
 {
     public class RepositorioBeneficiario : IRepositorioBeneficiario
     {
-        //HogarGestor.App.Dominio.Cls_Beneficiario IRepositorioBeneficiario.AddBeneficiario(HogarGestor.App.Dominio.Cls_Beneficiario beneficiario)
-        Cls_Beneficiario IRepositorioBeneficiario.AddBeneficiario(Cls_Beneficiario beneficiario)
+        private readonly AppContext _appContext;
+        public RepositorioBeneficiario(AppContext appContext)
         {
-            throw new System.NotImplementedException();
+            _appContext = appContext;
+        }
+        HogarGestor.App.Dominio.Cls_Beneficiario IRepositorioBeneficiario.AddBeneficiario(HogarGestor.App.Dominio.Cls_Beneficiario beneficiario)
+        {
+            var beneficiarioAdicionado = _appContext.beneficiarios.Add(beneficiario);
+            _appContext.SaveChanges();
+            return beneficiarioAdicionado.Entity;
         }
         void IRepositorioBeneficiario.DeleteBeneficiario(int IdBeneficiario)
         {
-            throw new System.NotImplementedException();
+            var beneficiarioEncontrado=_appContext.beneficiarios.FirstOrDefault(p=>p.Id==IdBeneficiario);
+            if(beneficiarioEncontrado==null)
+            return;
+            _appContext.beneficiarios.Remove(beneficiarioEncontrado);
+            _appContext.SaveChanges();
         }
         IEnumerable<Cls_Beneficiario> IRepositorioBeneficiario.GetAllBeneficiarios()
         {
-            throw new System.NotImplementedException();
+            return _appContext.beneficiarios;
         }
         Cls_Beneficiario IRepositorioBeneficiario.GetBeneficiario(int IdBeneficiario)
         {
-            throw new System.NotImplementedException();
+            return _appContext.beneficiarios.FirstOrDefault(p=>p.Id==IdBeneficiario);          
         }
         Cls_Beneficiario IRepositorioBeneficiario.UpdateBeneficiario(Cls_Beneficiario beneficiario)
         {
-            throw new System.NotImplementedException();
+            var beneficiarioEncontrado = _appContext.beneficiarios.FirstOrDefault(p=>p.Id==beneficiario.Id);
+            if(beneficiarioEncontrado != null)
+            {
+                beneficiarioEncontrado.nombre = beneficiario.nombre;
+                beneficiarioEncontrado.apellido = beneficiario.apellido;
+                beneficiarioEncontrado.documento = beneficiario.documento;
+                beneficiarioEncontrado.genero = beneficiario.genero;
+                beneficiarioEncontrado.latitud = beneficiario.latitud;
+                beneficiarioEncontrado.longitud = beneficiario.longitud;
+                beneficiarioEncontrado.ciudad = beneficiario.ciudad;
+                beneficiarioEncontrado.fechaNacimiento = beneficiario.fechaNacimiento;
+                beneficiarioEncontrado.familiar = beneficiario.familiar;
+                beneficiarioEncontrado.pediatra = beneficiario.pediatra;
+                beneficiarioEncontrado.nutricionista = beneficiario.nutricionista;
+                beneficiarioEncontrado.historiaClinica = beneficiario.historiaClinica;
+                _appContext.SaveChanges();
+           }
+           return beneficiarioEncontrado;
         }
-
     }
 }
