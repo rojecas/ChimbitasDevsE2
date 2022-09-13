cabea72e61f2f1f86402560b4741792d7d225c94
Naladestruction <111301518+Naladestruction@users.noreply.github.com>
2022-09-12 21:12:59 -0500

    Actualizacion de Web Crud Nutricionista

--- HogarGestor.app/HogarGestor.App.Presentacion/Pages/PersonalSalud/Create.cshtml.cs
+++ HogarGestor.app/HogarGestor.App.Presentacion/Pages/PersonalSalud/Create.cshtml.cs
@@ -1,12 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using HogarGestor.App.Dominio;
+using HogarGestor.App.Persistencia;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace HogarGestor.App.Presentacion.Pages_PersonalSalud
+namespace HogarGestor.App.Presentacion.Pages_PersonalSalud;
+
+public class CreateModel : PageModel
 {
-    public class CreateModel : PageModel
+    private readonly IRepositorioNutricionistaMemoria repositorioNutricionistaMemoria; // atributo de Clase
+    [BindProperty]
+    public Cls_PersonalSalud nutricionista { get; set; }
+    public CreateModel(IRepositorioNutricionistaMemoria repositorioNutricionistaMemoria) // Metodo constructor de la clase Create (modelo)
     {
-        public void OnGet()
-        {
-        }
+        this.repositorioNutricionistaMemoria = repositorioNutricionistaMemoria;
+    }
+    public void OnGet()
+    {
+
+    }
+    public IActionResult OnPostSave()
+    {
+        nutricionista = repositorioNutricionistaMemoria.Add(nutricionista);
+        return RedirectToPage("Index");
     }
 }
+
+
+
--- HogarGestor.app/HogarGestor.App.Presentacion/Pages/PersonalSalud/Details.cshtml.cs
+++ HogarGestor.app/HogarGestor.App.Presentacion/Pages/PersonalSalud/Details.cshtml.cs
@@ -1,12 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using HogarGestor.App.Dominio;
+using HogarGestor.App.Persistencia;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace HogarGestor.App.Presentacion.Pages_PersonalSalud
+namespace HogarGestor.App.Presentacion.Pages_PersonalSalud;
+
+public class DetailsModel : PageModel
 {
-    public class DetailsModel : PageModel
+    private readonly IRepositorioNutricionistaMemoria repositorioNutricionistaMemoria; // atributo de Clase
+
+ public Cls_PersonalSalud nutricionista { get; set; }
+    public DetailsModel(IRepositorioNutricionistaMemoria repositorioNutricionistaMemoria) // Metodo constructor de la clase Details (modelo)
     {
-        public void OnGet()
-        {
-        }
+        this.repositorioNutricionistaMemoria = repositorioNutricionistaMemoria;
+    }
+    public IActionResult OnGet(int Id)
+    {
+        nutricionista=repositorioNutricionistaMemoria.Get(Id);
+            if(nutricionista==null)
+            {
+                return RedirectToPage("./NotFound");
+            }
+            else
+            {
+                return Page();
+            }
     }
 }
