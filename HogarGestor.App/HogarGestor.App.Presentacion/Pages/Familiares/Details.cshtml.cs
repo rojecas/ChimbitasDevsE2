fde48499a67682d3319dc67bca32d11cf6d38b89
rojecas <rojecas@gmail.com>
2022-09-12 20:33:50 -0500

    Actualizacion PaginaWeb Detalles Familiares

--- HogarGestor.App/HogarGestor.App.Presentacion/Pages/Familiares/Details.cshtml.cs
+++ HogarGestor.App/HogarGestor.App.Presentacion/Pages/Familiares/Details.cshtml.cs
@@ -1,12 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using HogarGestor.App.Dominio;
+using HogarGestor.App.Persistencia;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace HogarGestor.App.Presentacion.Pages_Familiares
+namespace HogarGestor.App.Presentacion.Pages_Familiares;
+
+public class DetailsModel : PageModel
 {
-    public class DetailsModel : PageModel
+    private readonly IRepositorioFamiliarMemoria repositorioFamiliarMemoria; // atributo de Clase
+
+    public Cls_Familiar familiar { get; set; }
+    public DetailsModel(IRepositorioFamiliarMemoria repositorioFamiliarMemoria) // Metodo constructor de la clase Details (modelo)
     {
-        public void OnGet()
-        {
-        }
+        this.repositorioFamiliarMemoria = repositorioFamiliarMemoria;
+    }
+    public IActionResult OnGet(int Id)
+    {
+        familiar=repositorioFamiliarMemoria.Get(Id);
+            if(familiar==null)
+            {
+                return RedirectToPage("./NotFound");
+            }
+            else
+            {
+                return Page();
+            }
     }
 }
