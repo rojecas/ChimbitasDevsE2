b7047be2d47c009a2e651d5fa83d00fc6b13b850
Inominall <coragrande@hotmail.com>
2022-09-13 21:07:39 -0500

    agrego detalle IRepositorio Pediatra
    agregue detalle e tres puntos en nota de IRepositorio Pediatra

--- HogarGestor.app/HogarGestor.App.Persistencia/AppRepositorioMemoria/IRepositorioPediatraMemoria.cs
+++ HogarGestor.app/HogarGestor.App.Persistencia/AppRepositorioMemoria/IRepositorioPediatraMemoria.cs
@@ -1,7 +1,21 @@
-namespace HogarGestor.App.Persistencia.AppRepositorioMemoria
+
+using System.Collections;
+using System.Security.Cryptography.X509Certificates;
+using System.Collections.Generic;
+using HogarGestor.App.Dominio;
+
+namespace HogarGestor.App.Persistencia;
+public interface IRepositorioPediatraMemoria // No es una clase, es una interfaz...
 {
-    public class IRepositorioPediatraMemoria
-    {
-        
-    }
-}
+    IEnumerable<Cls_PersonalSalud> GetAll();
+
+    Cls_PersonalSalud Add(Cls_PersonalSalud pediatra);
+
+   Cls_PersonalSalud Update(Cls_PersonalSalud pediatra);
+
+    void Delete(int idPediatra);
+
+    Cls_PersonalSalud Get(int idPediatra);
+    IEnumerable<Cls_PersonalSalud>GetFilter(string filtro);
+} 
+    
