8af217e18af2a772a730a8f786163cbd6c3e69fe
rojecas <rojecas@gmail.com>
2022-09-15 22:42:10 -0500

    NuevaClase Asignar Medico

---
+++ HogarGestor.App/HogarGestor.App.Presentacion/Pages/Beneficiarios/AsigPerSalud.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using HogarGestor.App.Dominio;
+using HogarGestor.App.Persistencia;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HogarGestor.App.Presentacion.Pages_Beneficiarios;
+
+    public class AsigPerSaludModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
